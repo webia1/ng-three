a45b3fec997fb688db119b790736f016817f4601
webia1 <webia1.github@gmail.com>
2024-09-10 12:26:19 +0200

    Add Linter Configs and an Example Component in Angular App

--- apps/ng-three/src/app/app.component.ts
+++ apps/ng-three/src/app/app.component.ts
@@ -1,14 +1,16 @@
+import { NgtCanvas } from 'angular-three';
+import { Experience } from './experience/experience.component';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NgtCanvas],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-three';
+  sceneGraph = Experience;
 }
