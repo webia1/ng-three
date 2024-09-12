afaa52cac47b233d767a7af67d650291f177ce35
webia1 <webia1.github@gmail.com>
2024-09-12 14:46:00 +0200

    Update nx.json to include style configuration for "@nx/angular:component" Add coordinates component files

---
+++ apps/ng-three/src/app/shared/components/coordinates/coordinates.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-coordinates',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './coordinates.component.html',
+  styleUrl: './coordinates.component.scss',
+})
+export class CoordinatesComponent {}
